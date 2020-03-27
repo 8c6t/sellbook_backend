32bb7d7bca052ffc84b7f0dcd6144baa64219544
8c6t <nkw8620@gmail.com>
2020-03-27 15:25:05 +0900

    추가: BookRepository.findAllByIsbnIn 메소드
    검색 api 결과에서 isbn을 기준으로 저장할 책들을 필터링 하기 위해.

--- src/main/java/com/hachicore/sellbook/repository/BookRepository.java
+++ src/main/java/com/hachicore/sellbook/repository/BookRepository.java
@@ -3,6 +3,10 @@ package com.hachicore.sellbook.repository;
 import com.hachicore.sellbook.domain.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    List<Book> findAllByIsbnIn(Iterable<String> isbnList);
 
 }

09e3fb5e29d9c8d67b21a3418c674035fa1ebb44
8c6t <nkw8620@gmail.com>
2020-04-02 21:25:56 +0900

    추가: SignUpRequestValidator

--- src/main/java/com/hachicore/sellbook/controller/AccountController.java
+++ src/main/java/com/hachicore/sellbook/controller/AccountController.java
@@ -3,15 +3,14 @@ package com.hachicore.sellbook.controller;
 import com.hachicore.sellbook.config.security.jwt.JwtUtil;
 import com.hachicore.sellbook.controller.form.LoginRequest;
 import com.hachicore.sellbook.controller.form.SignUpRequest;
+import com.hachicore.sellbook.controller.form.SignUpRequestValidator;
 import com.hachicore.sellbook.domain.Account;
 import com.hachicore.sellbook.service.AccountService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.Errors;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -25,6 +24,12 @@ public class AccountController {
     private final AccountService accountService;
 
     private final JwtUtil jwtUtil;
+    private final SignUpRequestValidator signUpRequestValidator;
+
+    @InitBinder("signUpRequest")
+    public void initBinder(WebDataBinder webDataBinder) {
+        webDataBinder.addValidators(signUpRequestValidator);
+    }
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody LoginRequest loginRequest, HttpServletResponse response) {
@@ -42,8 +47,8 @@ public class AccountController {
             ResponseEntity.badRequest().body("올바른 값을 입력하세요");
         }
 
-        Account account = accountService.saveNewAccount(signUpRequest);
-        return ResponseEntity.ok().body(account);
+        accountService.saveNewAccount(signUpRequest);
+        return ResponseEntity.ok().build();
     }
 
 }

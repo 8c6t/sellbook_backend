29f918748195255995807e3498ba8dafa2b8da43
8c6t <nkw8620@gmail.com>
2020-04-04 19:56:31 +0900

    추가: 계정 생성 시 Storage 저장
    1:1 관계.

--- src/main/java/com/hachicore/sellbook/service/AccountService.java
+++ src/main/java/com/hachicore/sellbook/service/AccountService.java
@@ -3,7 +3,9 @@ package com.hachicore.sellbook.service;
 import com.hachicore.sellbook.controller.form.LoginRequest;
 import com.hachicore.sellbook.controller.form.SignUpRequest;
 import com.hachicore.sellbook.domain.Account;
+import com.hachicore.sellbook.domain.Storage;
 import com.hachicore.sellbook.repository.AccountRepository;
+import com.hachicore.sellbook.repository.StorageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +17,8 @@ import org.springframework.transaction.annotation.Transactional;
 public class AccountService {
 
     private final AccountRepository accountRepository;
+    private final StorageRepository storageRepository;
+
     private final PasswordEncoder passwordEncoder;
 
     public Account login(LoginRequest loginRequest) {
@@ -42,7 +46,17 @@ public class AccountService {
                 .password(passwordEncoder.encode(signUpRequest.getPassword()))
                 .build();
 
-        return accountRepository.save(account);
+        Account savedAccount = accountRepository.save(account);
+
+        saveStorage(savedAccount);
+
+        return savedAccount;
+    }
+
+    private void saveStorage(Account account) {
+        Storage storage = Storage.builder().build();
+        storage.addAccount(account);
+        storageRepository.save(storage);
     }
 
 }

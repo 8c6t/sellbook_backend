5a23fd17aaca6a621f7b9dfc08a06bf4d58a7e81
8c6t <nkw8620@gmail.com>
2020-04-14 02:26:17 +0900

    수정: isbn13이 비었을 경우 isbn으로 대체

--- src/main/java/com/hachicore/sellbook/api/search/aladin/dto/AladinBookResponse.java
+++ src/main/java/com/hachicore/sellbook/api/search/aladin/dto/AladinBookResponse.java
@@ -4,6 +4,7 @@ import com.hachicore.sellbook.api.search.dto.BookResponse;
 import com.hachicore.sellbook.domain.Book;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.util.StringUtils;
 
 @Getter @Setter
 public class AladinBookResponse implements BookResponse {
@@ -11,6 +12,7 @@ public class AladinBookResponse implements BookResponse {
     private String title;
     private String author;
     private String publisher;
+    private String isbn;
     private String isbn13;
     private Integer priceStandard;
     private String cover;
@@ -21,7 +23,7 @@ public class AladinBookResponse implements BookResponse {
                 .title(title)
                 .author(author)
                 .publisher(publisher)
-                .isbn(isbn13)
+                .isbn(StringUtils.isEmpty(isbn13) ? isbn : isbn13)
                 .price(priceStandard)
                 .image(cover)
                 .build()

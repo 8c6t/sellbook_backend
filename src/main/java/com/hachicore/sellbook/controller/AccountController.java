0b493984104d1d70bd731fd10ecda853060a5d3d
8c6t <nkw8620@gmail.com>
2020-04-15 20:18:44 +0900

    수정: 회원가입 시 쿠키 발행

--- src/main/java/com/hachicore/sellbook/controller/AccountController.java
+++ src/main/java/com/hachicore/sellbook/controller/AccountController.java
@@ -44,14 +44,18 @@ public class AccountController {
     }
 
     @PostMapping("/account")
-    public ResponseEntity signUp(@RequestBody @Valid SignUpRequest signUpRequest, Errors errors) {
+    public ResponseEntity signUp(@RequestBody @Valid SignUpRequest signUpRequest, Errors errors, HttpServletResponse response) {
         if (errors.hasErrors()) {
             // TODO 2020.04.15 별도 에러 객체로 json 형태 리턴할 수 있도록...
             return ResponseEntity.badRequest().body("올바른 값을 입력하세요");
         }
 
-        accountService.saveNewAccount(signUpRequest);
-        return ResponseEntity.ok().build();
+        Account account = accountService.saveNewAccount(signUpRequest);
+
+        Cookie cookie = jwtUtil.generateAccessTokenCookie(account);
+        response.addCookie(cookie);
+
+        return ResponseEntity.ok().body(new AccountDto(account));
     }
 
     @GetMapping("/account")
